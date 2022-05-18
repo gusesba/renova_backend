6f4c831850f90bd3b6ca6680837cfd3798d77c30
gusesba <gustavoesmanhotto@hotmail.com>
2022-05-18 19:14:13 -0300

    .env

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "password",
-  database: "sqlnode",
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DATABASE,
   define: {
     timestamps: true,
     underscorerd: true,
