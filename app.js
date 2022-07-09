a1f87ec9b950c5e1776bbddac957e7890e5f99a6
gusesba <gustavoesmanhotto@hotmail.com>
2022-07-09 18:48:27 -0300

    cors

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const notFound = require("./middleware/not-found");
 const port = process.env.PORT || 5000;
 
 require("./db/index");
-app.use(cors());
+
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/clients", clients);
 app.use("/api/v1/products", products);
