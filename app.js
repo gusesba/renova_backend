0fb2b20543796f07be317c4c3b39892845dce574
gusesba <gustavoesmanhotto@hotmail.com>
2022-07-09 19:34:49 -0300

    cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+
 const clients = require("./routes/clients");
 const products = require("./routes/products");
 const sells = require("./routes/sells");
@@ -9,7 +9,15 @@ const notFound = require("./middleware/not-found");
 const port = process.env.PORT || 5000;
 
 require("./db/index");
-
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 
 app.use("/api/v1/clients", clients);
